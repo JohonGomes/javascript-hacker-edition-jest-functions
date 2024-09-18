70ca8d5c0b75df53dd97944eed6a81b307d9f6cd
Johon <johon.gomes@hotmail.com>
2024-09-18 19:58:42 -0300

    Resolução

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 // Função que soma dois números
-function somar() {}
+function somar(n1, n2) {
+  return n1 + n2;
+}
 
 // Função que subtrai dois números
-function subtrair() {}
+function subtrair(n1, n2) {
+  return n1 - n2;
+}
 
 // Função que multiplica dois números
-function multiplicar() {}
+function multiplicar(n1,n2) {
+  return n1 - n2;
+}
 
 // Função que divide dois números, com verificação de divisão por zero
-function dividir() {}
+function dividir(n1,n2) {
+  return n1 / n2;
+}
 
 // Função que verifica se um número é par
-function ehPar() {}
+function ehPar(num) {
+  return num % 2 === 0;
+}
+
+
 
 // Exporta todas as funções para serem usadas nos testes
 module.exports = {
